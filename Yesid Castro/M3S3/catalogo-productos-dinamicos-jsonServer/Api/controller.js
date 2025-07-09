0ba52b5bde6031b55548de312ece525e6e0db374
rogelisdev <rogelis.taks@gmail.com>
2025-07-09 11:09:51 -0500

    .

--- Yesid Castro/M3S3/catalogo-productos-dinamicos-jsonServer/Api/controller.js
+++ Yesid Castro/M3S3/catalogo-productos-dinamicos-jsonServer/Api/controller.js
@@ -1,44 +1,14 @@
-import { loginUser, getToken, logoutUser } from "./model.js";
-import { showLogin, showDashboard } from "./view.js";
+import { getProducts} from "./model.js";
+import { showCard } from "./view.js";
 
 const app = document.getElementById("card");
 
-async function onLogin(event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const token = await loginUser(username, password);
-    try {
-        if (token) {
-            renderDashboard();
-        } else {
-            throw new Error("Invalid credentials");
-        }
-    } catch (error) {
-        alert("Login failed: " + error.message);
-    }
-    
+async function init() {
+    const productos = await getProducts();
+    showCard(app, productos);
 }
 
-async function onLogout() {
-    try {
-        await logoutUser();
-        renderLogin();
-    } catch (error) {
-        alert("Logout failed: " + error.message);
-    }
-}
-
-function renderLogin() {
-    showLogin(app, onLogin);
-}
-
-function renderDashboard() {
-    showDashboard(app, onLogout);
-}
-
-if (getToken()) {
-    renderDashboard();
-} else {
-    renderLogin();
-}
+init().catch(error => {
+    console.error("Error al inicializar la aplicación:", error);
+    app.innerHTML = "<p>Error al cargar los productos.</p>";
+});
--- Yesid Castro/M3S3/catalogo-productos-dinamicos-jsonServer/Api/model.js
+++ Yesid Castro/M3S3/catalogo-productos-dinamicos-jsonServer/Api/model.js
@@ -1,18 +1,5 @@
-export async function loginUser(username, password) {
-    const response = await fetch(`http://localhost:3000/users?username=${username}&password=${password}`);
-    const users = await response.json();
-    if (users.length > 0){
-        const token = btoa(users[0].token);
-        localStorage.setItem("token", token);
-        return token;
-    }
-    return null;
-}
-
-export function getToken(){
-    return localStorage.getItem("token");
-}
-
-export function logoutUser() {
-    localStorage.removeItem("token")
+export async function getProducts() {
+    const response = await fetch(`http://localhost:3000/productos`);
+    const products = await response.json();
+    return products;
 }
